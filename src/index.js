7910591ed0ae611165a8b0875291b69aed0e03f6
Sebastian Figueroa <s.figueroa.c@hotmail.com>
2020-08-17 08:44:57 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+//var React = require("react"); // OLD STYLE
+//var ReactDOM = require("react-dom"); // OLD STYLE
+
+ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"));
+
+/* OLD STYLE */
+//Do not use. Just to compare
+//var h1 = document.createElement("h1");
+//h1.innerHTML = "Hello World 2";
+//document.getElementById("root").appendChild(h1);
